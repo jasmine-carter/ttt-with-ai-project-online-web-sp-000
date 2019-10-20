57134e27eb30355b100dedabc15e26e8662b9937
Jasmine Carter <jasmine.carter87@gmail.com>
2019-10-20 22:21:08 +0000

    Automatically backed up by Learn

--- lib/game.rb
+++ lib/game.rb
@@ -58,9 +58,9 @@ attr_accessor :board, :player_1, :player_2
 
   def turn
     player = current_player
-    current_move = player.move
+    current_move = player.move(input)
     if !board.valid_move?(current_move)
-      current_move =player.move
+      current_move =player.move(input)
     else
       board.update(current_move, current_player)
     end
